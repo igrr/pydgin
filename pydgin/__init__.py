1a80645843d70fd99530298b1cd7971bfd64ebf3
Ivan Grokhotkov <ivan@espressif.com>
2020-07-06 17:33:48 +0200

    export some useful functions

--- pydgin/__init__.py
+++ pydgin/__init__.py
@@ -1,4 +1,4 @@
-from .storage import RAM, AddressSpace, MMIO, MemError, MemErrorCatcher
+from .storage import RAM, AddressSpace, MMIO, MemError, MemErrorCatcher, MemoryLike
 from .misc import load_program
 from .debug import Debug
 from .machine import Machine
--- pydgin/arm/__init__.py
+++ pydgin/arm/__init__.py
@@ -1,3 +1,4 @@
 from .isa import decode
 from .instruction import Instruction
 from .machine import State, ArmRegisterFile
+from .utils import popcount
